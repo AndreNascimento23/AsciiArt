25c545f91d85281b5ad49294f8e114f15d589bde
André Nascimento <andre4.20nascimento@outlook.com>
2023-03-07 11:01:50 -0300

    Aranha e Mosca

---
+++ Program.cs
@@ -0,0 +1,70 @@
+﻿Console.Clear();
+Console.WriteLine();
+Console.WriteLine("  ||  ||    *"); 
+Console.Write("  \\");
+Console.WriteLine("\\()//");
+Console.Write(" //(__)\\");
+Console.WriteLine("\\ ");
+Console.WriteLine("||      || ");
+Console.WriteLine(); Thread.Sleep(1000);
+
+Console.Clear();
+Console.WriteLine();
+Console.WriteLine("   ||  ||   *");
+Console.Write("   \\");
+Console.WriteLine("\\()//");
+Console.Write("  //(__)\\");
+Console.WriteLine("\\ ");
+Console.WriteLine(" ||      || ");
+Console.WriteLine(); Thread.Sleep(1000);
+
+Console.Clear();
+Console.WriteLine(); 
+Console.WriteLine("    ||  ||  *");
+Console.Write("    \\");
+Console.WriteLine("\\()//");
+Console.Write("   //(__)\\");
+Console.WriteLine("\\ ");
+Console.WriteLine("  ||      || ");
+Console.WriteLine(); Thread.Sleep(1000);
+
+Console.Clear();
+Console.WriteLine(); 
+Console.WriteLine("     ||  || *");
+Console.Write("     \\");
+Console.WriteLine("\\()//");
+Console.Write("    //(__)\\");
+Console.WriteLine("\\ ");
+Console.WriteLine("   ||      || ");
+Console.WriteLine(); Thread.Sleep(1000);
+
+Console.Clear();
+Console.WriteLine(); 
+Console.WriteLine("      ||  ||* ");
+Console.Write("      \\");
+Console.WriteLine("\\()//");
+Console.Write("     //(__)\\");
+Console.WriteLine("\\ ");
+Console.WriteLine("    ||      || ");
+Console.WriteLine(); Thread.Sleep(1000);
+
+Console.Clear();
+Console.WriteLine(); 
+Console.WriteLine("      ||  * || ");
+Console.Write("       \\");
+Console.WriteLine("\\()//"); 
+Console.Write("      //(__)\\");
+Console.WriteLine("\\ ");
+Console.WriteLine("     ||      || ");
+Console.WriteLine(); Thread.Sleep(1000);
+
+Console.Clear(); 
+Console.WriteLine("       ||   || ");
+Console.Write("       \\");
+Console.WriteLine("\\(*)//"); 
+Console.Write("      //(_ _)\\");
+Console.WriteLine("\\ ");
+Console.WriteLine("     ||       || ");
+Console.WriteLine();  Thread.Sleep(1000);
+
+Console.WriteLine("Obrigado por Visualizar"); Console.WriteLine();
